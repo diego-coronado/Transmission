edcbe844770738fc14a395cf11eaf3e1e2116584
d_coronado <diego.cor.mik@gmail.com>
2018-01-26 21:32:54 -0500

    initial commit

---
+++ Transmission/Assets/Scripts/EnergyLink.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyLink : MonoBehaviour {
+    public Transform first;
+    public Transform second;
+    private LineRenderer _lineRenderer;
+    // Use this for initialization
+    void Start() {
+        _lineRenderer = GetComponent<LineRenderer>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        float distance = Vector3.Distance(first.position, second.position);
+        Debug.Log(distance);
+        if (distance < 5)
+        {
+            _lineRenderer.enabled = true;
+            _lineRenderer.SetPosition(0, first.position);
+            _lineRenderer.SetPosition(1, second.position);
+            _lineRenderer.SetPosition(2, second.position);
+        }else
+        {
+            _lineRenderer.enabled = false;
+        }
+       
+	}
+}
---
+++ Transmission/Assets/Scripts/MovementControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+public class MovementControl : MonoBehaviour {
+    public int _playerId = 0;
+    public float _speed = 5;
+    private Player _input;
+    private float _h;
+    private float _v;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 5;
+    }
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(Application.targetFrameRate);
+        _input = ReInput.players.GetPlayer(0);
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (_playerId == 0)
+        {
+            _h = _input.GetAxis("MoveLeftX");
+            _v = _input.GetAxis("MoveLeftY");
+        }
+        else
+        {   
+            _h = _input.GetAxis("MoveRightX");
+            _v = _input.GetAxis("MoveRightY");
+        }
+
+        Vector2 movement = new Vector3(_h, _v, 0);
+        movement *= _speed * Time.deltaTime;
+        transform.Translate(movement);
+
+        
+    
+        
+
+    }
+}
