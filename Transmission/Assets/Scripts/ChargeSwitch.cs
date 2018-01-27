1e31ba2f15d40165f7899576121b22d4966d781f
Diego Coronado <diegocoronado8@hotmail.com>
2018-01-27 16:15:03 -0500

    charge bar en el switch

--- Transmission/Assets/Scripts/ChargeSwitch.cs
+++ Transmission/Assets/Scripts/ChargeSwitch.cs
@@ -9,13 +9,16 @@ public class ChargeSwitch : MonoBehaviour
     public float chargeIncreaseSpeed = 2.5f;
     public float chargeReductionSpeed = 0.5f;
     public float maxCharge = 100;
+    public string OnChargedMessage = "";
 
     private float _lastLinkActivatedTime;
     private MovementControl _player = null;
-    private float _energyCharged;
+    private float _energyCharged = 0;
     private float timeDif;
     private bool _isLinkPressed = false;
     private Player _input;
+    private bool _hasPlayerLeft = false;
+    private bool _finishedCharging = false;
 
     private void Start()
     {
@@ -25,6 +28,10 @@ public class ChargeSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_hasPlayerLeft && _energyCharged>0 && !_finishedCharging)
+        {
+            _energyCharged -= Time.deltaTime * chargeReductionSpeed;
+        }
 
         if (_player)
         {
@@ -46,7 +53,7 @@ public class ChargeSwitch : MonoBehaviour
             if (_isLinkPressed)
             {
                 timeDif = Mathf.Abs(Time.timeSinceLevelLoad - _player.timeSinceLinkActivated);
-                if (timeDif >= maxTimeDifference)
+                if (timeDif >= maxTimeDifference && !_finishedCharging)
                 {
                     Debug.Log("disminucion de carga");
                     _energyCharged -= Time.deltaTime * chargeReductionSpeed;
@@ -63,6 +70,16 @@ public class ChargeSwitch : MonoBehaviour
             Debug.Log("time diff: " + timeDif);
             Debug.Log("aumento de carga");
             _energyCharged += chargeIncreaseSpeed;
+
+            if (_energyCharged > maxCharge)
+            {
+                if (OnChargedMessage != "")
+                {
+                    _finishedCharging = true;
+                    SendMessage(OnChargedMessage);
+                }
+            }
+
             _isLinkPressed = true;
             Debug.Log("Energy charged: " + _energyCharged);
         }
@@ -73,7 +90,7 @@ public class ChargeSwitch : MonoBehaviour
         if (other.tag == "Player")
         {
             _player = other.GetComponent<MovementControl>();
-            _energyCharged = 0;
+            _hasPlayerLeft = false;
         }
     }
 
@@ -81,9 +98,13 @@ public class ChargeSwitch : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            _player = null;
-            _energyCharged = 0;
+            _hasPlayerLeft = true;
             _isLinkPressed = false;
         }
+    }
+
+    public float GetChargePercentage()
+    {
+        return _energyCharged / maxCharge;
     }
 }
---
+++ Transmission/Assets/Scripts/GateBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateBar : MonoBehaviour {
+
+	public ChargeSwitch gateSwitch;
+
+    private void Update()
+    {
+        transform.localScale = new Vector3(Mathf.Clamp(gateSwitch.GetChargePercentage(), 0, 1), 1, 0);
+    }
+}
