d4b6ec80f1554cb5e026dca085d05dad25c5ae6a
Diego Coronado <diegocoronado8@hotmail.com>
2018-01-27 21:11:06 -0500

    timer para el lvl y lvlloader

--- Transmission/Assets/Scripts/CameraControl.cs
+++ Transmission/Assets/Scripts/CameraControl.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraControl : MonoBehaviour {
+    public Text timerText;
+
 	private bool _moving = false;
 	private Vector3 _targetPos;
+    private float _timer;
+    public float _maxTime = 30;
+    private bool _runTimer;
+
 	// Use this for initialization
 	void Start () {
-	
+        _runTimer = true;
+        _timer = _maxTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (_runTimer)
+        {
+            _timer -= Time.deltaTime;
+            Debug.Log(_timer);
+            timerText.text = "Tiempo para terminar: "+_timer;
+            if (_timer <= 0)
+            {
+                GameOver();
+            }
+        }
+
 		if (_moving) {
 			Vector3 newPos = Vector3.Lerp (transform.position, _targetPos, Time.deltaTime * 10);
 			float distSqr = Vector3.SqrMagnitude (newPos - _targetPos);
@@ -26,5 +46,18 @@ public class CameraControl : MonoBehaviour {
 	public void MoveToNextLevel(){
 		_moving = true;
 		_targetPos = transform.position + (Vector3.up * 10);
+        int level = PlayerPrefs.GetInt("Level", 0);
+        PlayerPrefs.SetInt("Level", level + 1);
+        CancelInvoke("GameOver");
+        Invoke("GameOver", _maxTime);
+        _runTimer = true;
+        _timer = _maxTime;
+
 	}
+
+    private void GameOver()
+    {
+        _runTimer = false;
+        SceneManager.LoadScene(0);
+    }
 }
---
+++ Transmission/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject mainCamera;
+    public Transform player1;
+    public Transform player2;
+
+    private void Awake()
+    {
+        float level = PlayerPrefs.GetInt("Level", 0);
+        Debug.Log(level);
+        mainCamera.transform.position = new Vector3(0, 10 * level, -10);
+        player1.position = new Vector3(4, 10 * level -4, 0);
+        player2.position = new Vector3(-4, 10 * level -4, 0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            PlayerPrefs.SetInt("Level", 0);
+        }
+    }
+}
